a40cf2b26af603cde2f5bfe80d8756bad84a05d0
Yahel972 <yahelbareket@gmail.com>
2021-12-02 17:51:15 +0200

    Added docstrings

--- Chess project/Source files/Piece.cpp
+++ Chess project/Source files/Piece.cpp
@@ -1,14 +1,17 @@
 #include "Piece.h"
 
+// constructor
 Piece::Piece(char type)
 {
     this->_type = type;
 }
 
+// destructor (using default)
 Piece::~Piece()
 {
 }
 
+// type getter
 char Piece::getType() const
 {
     this->_type;
