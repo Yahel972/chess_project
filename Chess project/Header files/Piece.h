853fa783fe1d33912ddf82883e6c238e2309edf1
Nehoray Bitan <bvurthc@gmail.com>
2021-12-03 00:14:25 +0200

    added game codes enum

--- Chess project/Header files/Piece.h
+++ Chess project/Header files/Piece.h
@@ -4,12 +4,14 @@
 
 class Board;
 
+enum gameCodes { validMove, checkOnEnemy, srcInvalid, dstInvalid, invalidCheckOnSelf, invalidIndex, invalidMove, invalidSrcIsDst, checkMate };
+
 class Piece
 {
 public:
 	Piece(char type);
 	virtual ~Piece();
-	virtual int checkMove(const std::string& currentPlace, const std::string& newPlace, const Board& board) const = 0;
+	virtual gameCodes checkMove(const std::string& newPlace, const Board& board) const = 0;
 	char getType() const;
 protected:
 	char _type;
