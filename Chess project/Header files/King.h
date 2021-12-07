2a3ae35aebbb309b5dd99cb04d0b18260e825c46
Nehoray Bitan <bvurthc@gmail.com>
2021-12-07 18:33:00 +0200

    header file updated

--- Chess project/Header files/King.h
+++ Chess project/Header files/King.h
@@ -7,4 +7,6 @@ public:
 	King(char type, std::string _currPlace);
 	virtual ~King();
 	virtual gameCodes checkMove(const std::string& newPlace, const Board& board) const;
+	static bool isKingThreatened(char type);
+	static bool isCheckMate(char type);
 };
