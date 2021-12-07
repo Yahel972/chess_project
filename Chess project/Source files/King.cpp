2fc6d04045bb874f69e6a8533c568f162cb812f1
Nehoray Bitan <bvurthc@gmail.com>
2021-12-07 18:32:32 +0200

    added empy methods to be implemented

--- Chess project/Source files/King.cpp
+++ Chess project/Source files/King.cpp
@@ -8,3 +8,17 @@ gameCodes King::checkMove(const std::string& newPlace, const Board& board) const
 {
 	return gameCodes::validMove;
 }
+
+bool King::isKingThreatened(char type /* k to check for black king, K for the white */)
+{
+	//TODO: check if king is being threatened by any piece. return true if the king is threatened, else return false.
+	return false;
+}
+
+bool King::isCheckMate(char type /* k to check for black king, K for the white */)
+{
+	//TODO(optinal): check if the king cant move anywhere. return true if the king is threatened, else return false.
+	//implementation suggestion: change the king location to any valid location,
+	//and call isKingThreatened() every time to check if there is any move possible.
+	return false;
+}
