4d17e8b80a74ff2a21a5ebe2b9887b1a56b07b89
Nehoray Bitan <bvurthc@gmail.com>
2021-12-07 22:41:08 +0200

    added some docs, fixed a possible bug in Piece::isIndexValid()

--- Chess project/Source files/Piece.cpp
+++ Chess project/Source files/Piece.cpp
@@ -18,18 +18,18 @@ char Piece::getType() const
 {
     return this->_type;
 }
-
+// currPlace getter
 std::string Piece::getCurrPlace() const
 {
     return _currPlace;
 }
-
+// currPlace setter
 void Piece::setCurrPlace(std::string newVal)
 {
     _currPlace = newVal;
 }
-
+// checking if an index is valid
 bool Piece::isIndexValid(std::string index)
 {
-    return !index[2] && isalpha(index[0]) && isdigit(index[1]) && index[1] - '0' <= SIDE_SIZE && index[0] < 'a' + SIDE_SIZE && index[1] - '0' >= 1 && index[0] - 'a' >= 0;
+    return index.length() == 2 && isalpha(index[0]) && isdigit(index[1]) && index[1] - '0' <= SIDE_SIZE && index[0] < 'a' + SIDE_SIZE && index[1] - '0' >= 1 && index[0] - 'a' >= 0;
 }
