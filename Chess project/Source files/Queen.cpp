e8641cbcd6b178e840d3c3c52b184873e27d74c4
Nehoray Bitan <bvurthc@gmail.com>
2021-12-14 19:12:32 +0200

    added code for Queen::checkMove()

--- Chess project/Source files/Queen.cpp
+++ Chess project/Source files/Queen.cpp
@@ -1,10 +1,16 @@
 #include "Queen.h"
 
-Queen::Queen(char type, std::string _currPlace): Piece(type, _currPlace) {}
+Queen::Queen(char type, std::string _currPlace): Piece(type, _currPlace), Rook(type, _currPlace), Bishop(type, _currPlace) {}
 
 Queen::~Queen() {}
 
 gameCodes Queen::checkMove(const std::string& newPlace, const Board& board, bool dontRecurse) const
 {
-	return gameCodes::checkMate;
+	//checking if move is linear, and if it is, treating it as a move of a Rook
+	if (newPlace[0] == _currPlace[0] || newPlace[1] == _currPlace[1])
+	{
+		return Rook::checkMove(newPlace, board, dontRecurse);
+	}
+	//else treating it as move of a Bishop 
+	return Bishop::checkMove(newPlace, board, dontRecurse);
 }
