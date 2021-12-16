2415d8d278ef2ab03475b32788ca2d4e51cf9b97
Yahel972 <yahelbareket@gmail.com>
2021-12-16 18:36:52 +0200

    Added  Knight::checkMove()

--- Chess project/Source files/Knight.cpp
+++ Chess project/Source files/Knight.cpp
@@ -6,5 +6,46 @@ Knight::~Knight() {}
 
 gameCodes Knight::checkMove(const std::string& newPlace, const Board& board, bool dontRecurse) const
 {
-	return gameCodes::checkMate;
+	if (this->_currPlace == newPlace) return gameCodes::invalidSrcIsDst;  // checking if the new place is same as the current place
+
+	if (!Piece::isIndexValid(newPlace)) return gameCodes::invalidIndex;  // checking if the index given is valid
+
+	char currentXIndex = this->_currPlace[0];
+	char currentYIndex = this->_currPlace[1];
+	char newXIndex = newPlace[0];
+	char newYIndex = newPlace[1];
+
+	const char* newPlaceArr = newPlace.data();
+
+	// checking if the move is valid:
+	if ((int(currentXIndex) == int(newXIndex) + 2 && int(currentYIndex) == int(newYIndex) + 1) ||
+		(int(currentXIndex) == int(newXIndex) + 2 && int(currentYIndex) == int(newYIndex) - 1) ||
+		(int(currentXIndex) == int(newXIndex) - 2 && int(currentYIndex) == int(newYIndex) + 1) ||
+		(int(currentXIndex) == int(newXIndex) - 2 && int(currentYIndex) == int(newYIndex) - 1) ||
+		(int(currentXIndex) == int(newXIndex) + 1 && int(currentYIndex) == int(newYIndex) + 2) ||
+		(int(currentXIndex) == int(newXIndex) + 1 && int(currentYIndex) == int(newYIndex) - 2) ||
+		(int(currentXIndex) == int(newXIndex) - 1 && int(currentYIndex) == int(newYIndex) + 2) ||
+		(int(currentXIndex) == int(newXIndex) - 1 && int(currentYIndex) == int(newYIndex) - 2))
+	{
+		// checking if the knight is stepping on a place that there is already a piece in his team:
+		if (board[newPlaceArr] && ((isupper(board[newPlaceArr]->getType()) && isupper(this->_type)) || (islower(board[newPlaceArr]->getType()) && islower(this->_type))))
+			return gameCodes::dstInvalid;
+		
+		if (dontRecurse)
+			return gameCodes::validMove;
+		
+		//checking if move causes self king to be threatened
+		if ((King::isKingThreatened(' ', board, newPlace)))
+			return gameCodes::invalidCheckOnSelf;
+
+		if (King::isCheckMate(this->_type))
+			return gameCodes::checkMate;
+		
+		//checking if the move caused a check on the other king and NOT checkMate: just a check
+		if ((King::isKingThreatened(' ', board, newPlace)) && !King::isCheckMate(this->_type))
+			return gameCodes::checkOnEnemy;
+	}
+	else return gameCodes::invalidMove;
+
+	return gameCodes::validMove;
 }
