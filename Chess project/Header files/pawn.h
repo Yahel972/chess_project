66b177cf337ece64ae108d81c302e0b5039a867d
Yahel972 <yahelbareket@gmail.com>
2021-12-17 00:52:33 +0200

    Added Pawn::checkMove()

--- Chess project/Header files/pawn.h
+++ Chess project/Header files/pawn.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Piece.h"
+#include "King.h"
 
 class Pawn: public Piece
 {
@@ -7,4 +8,7 @@ public:
 	Pawn(char type, std::string _currPlace);
 	virtual ~Pawn();
 	virtual gameCodes checkMove(const std::string& newPlace, const Board& board, bool dontRecurse = false) const override;
+	
+private:
+	bool _isFirstMove;
 };
